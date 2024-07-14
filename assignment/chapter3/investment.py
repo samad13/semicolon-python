bd9f932fbdec17c30107092d2aafe86c4e0b7a23
samad13 <yusufabdulsamad93@gmail.com>
2024-07-14 23:58:25 +0100

    chapter3

--- assignment/chapter3/investment.py
+++ assignment/chapter3/investment.py
@@ -1,7 +1,7 @@
 money = int(input('input your initial investment: '))
 investment=0
 annual_interest = 0.07
-for len in range(1,30 +1):
-	investment +=  money * (1 + annual_interest) ** len
+for len in range(1,31):
+	investment +=  money * (1 + annual_interest) * len
 	print(f"At the end of year {len}, you will have ${investment:.2f}")
 print(f"${investment:.2f}")
