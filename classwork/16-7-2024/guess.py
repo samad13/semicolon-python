e5fa1100c5a0b93ae7ae5622ed81b3173ea14004
samad13 <yusufabdulsamad93@gmail.com>
2024-07-16 17:35:54 +0100

    done

---
+++ classwork/16-7-2024/guess.py
@@ -0,0 +1,20 @@
+from random import randint
+
+number = randint(1,10)
+count = 0
+do_again = ""
+while(do_again != "no" ):
+	answer = int(input("Guess my number between 1 and 1000 with the fewest guesses: "))
+	count +=1
+	if(answer > number):
+		print("Too high.. Try again")
+	elif(answer < number):
+		print("Too low.. Try again")
+	elif(number == answer):	
+		if (count <= 10):
+			print(f"Congratulations. You guessed the number in {count} times!")
+		elif(count > 10):
+			print(f"Congratulations. You guessed the number in {count} times, you can try again next time!")
+		number = randint(1,10)
+		count -=count
+		do_again = input("input no  to stop or yes to continue: ")
