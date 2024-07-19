407505c30031cbe74826d95ae7ccbdd4358bc648
samad13 <yusufabdulsamad93@gmail.com>
2024-07-19 09:39:33 +0100

    done

--- assignment/17-7-2024/price.py
+++ assignment/17-7-2024/price.py
@@ -1,13 +1,13 @@
 price = int(input("what is the price of the car: "))
 
-if(price < 1000000):
-	print("your road tax is 10%")
+if(price <= 1000000):
+	print("tax is : ", price * 0.1);
 elif(price >= 1000000 and price <= 3000000 ):
-	print("your road tax is 15%")
+	print("tax is : ", price * 0.15)
 elif(price > 3000000 and price < 5000000 ):
-	print("your road tax is 20%")
+	print("tax is : ", price * 0.2)
 elif(price >= 5000000 ):
-	print("your road tax is 25%")
+	print("tax is : ", price * 0.25)
 
 
 
--- assignment/17-7-2024/sevenandfive.py
+++ assignment/17-7-2024/sevenandfive.py
@@ -1,3 +1,4 @@
-for number in range(770, 4200):
+for number in range(770, 4201):
 	if(number % 7 == 0 and number % 5 != 0):
 		print(number, end=', ')
+# and dont put coma to the last number
