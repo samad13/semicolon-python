54b394b52b464e829745822e131bf9046b8ded33
samad13 <yusufabdulsamad93@gmail.com>
2024-07-30 11:09:45 +0100

    2nd

--- Abdulsamad-Yusuf/part2/even.py
+++ Abdulsamad-Yusuf/part2/even.py
@@ -1,3 +1,18 @@
 for number in range (999, 3001):
-	if (number % 2==0):
-		print(number, end=', ')
+	
+	value1 = number // 1000
+	value2 = number % 10
+	placeholder2 = number // 10
+	placeholder1 = number // 100
+	value4 = placeholder2 % 10
+	value3 = placeholder1 % 10
+
+	if (value1 %2==0 or value2 % 2==0 or value3 % 2==0 or value4 % 2==0 ):
+		print(f"{value1}{value2}{value3}{value4,}")
+		
+
+
+
+
+
+	
---
+++ Abdulsamad-Yusuf/part2/oddoreven.py
@@ -0,0 +1,11 @@
+def oddoreven(number):
+	if number % 2 == 0:
+		return "it is an even number"
+	elif number % 2 != 0:
+		return "it is an odd number"
+
+
+
+
+print(oddoreven(21))
+
